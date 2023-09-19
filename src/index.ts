c7525cf215fc518c524df3d59a478e923cedd5df
Vitor Emanoel <manoelvitor132562@gmail.com>
2023-09-19 09:15:29 -0300

    ADD

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ const app = express()
 const router = Router()
 
 // Configurando o Express
-app.use(express.json()) // Configurando para receber JSON
+app.use(express.json()) // Configurando para reber JSON
 app.use(router)
 
+// GET, POST, OUT, DELETE, PATH
+
 router.get('/', (request, response) =>
-  response.json({ ola: 'Ola mundo', nome: 'Glêsio' }))
+  response.json({ ola: 'Ola mundo', nome: 'VITORZAAO O MAIS LINDO DA ESCOLA SESI CONSELHEIRO SARAIVA' }))
+
+router.post('/formulario', (request, response) => {
+  // nome, email
+  const { nome, email } = request.body
+  return response.json({ nome, email })
+})
 
 app.listen(3000, () => { console.log('Running port 3000') })
